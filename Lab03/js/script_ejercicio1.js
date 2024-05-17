c5ad41506111a91ee84a880b94bef649e3ef611e
DrN25 <rafael.nina123@gmail.com>
2024-05-17 16:50:34 -0500

    Creando un formulario en el html, el cual sera de ayuda al momento de enviar el valor colocado en el teclado al hacer click en el boton de Enviar. Tambien en el js se creo una funcion la cual en caso de que se haya enviado Enviar, se mostrara una alerta de la pagina, con el valor entregado

--- Lab03/js/script_ejercicio1.js
+++ Lab03/js/script_ejercicio1.js
@@ -1,3 +1,4 @@
+//Esta funcion desordena el arreglo de numeros para que esten randomizados
 function desordenarArray(arr) {
 	for(let i = 0; i < arr.length; i++) {
 		let random = Math.floor(Math.random() * arr.length);
@@ -7,28 +8,42 @@ function desordenarArray(arr) {
 	}
 	return arr;
 }
+//Esta funcion muestra el valor que se haya enviado en una ventana de alerta
+function alerta() {
+	let url = new URLSearchParams(window.location.search);
+	let contenido = url.get('barra');
+	if(contenido) {		//Si no se envio nada, no se mostrara la alerta
+		alert(`Enviado: ${contenido}`);
+	}
+}
 
 function teclado() {
 	let arr = [1,2,3,4,5,6,7,8,9,0];
 	arr = desordenarArray(arr);
 	let teclado = document.getElementById('teclado');
-	let barra = document.getElementById('barra');
+	let barra = document.getElementById('barra');	//Aqui se guardaran los datos ingresados
+	//Creando las teclas numericas y asignandoles un valor al ser presionadas
 	for(let i = 0; i < arr.length; i++) {
 		let tecla = document.createElement('button');
 		tecla.textContent = arr[i];
+		tecla.type = 'button';
 		teclado.appendChild(tecla);
 		tecla.addEventListener('click', ()=> {
 			barra.value += arr[i];
 		});
-		if( (i + 1) % 3 === 0) {
+		if( (i + 1) % 3 === 0) {	//Cada 3 botones se hara salto de linea
 			teclado.appendChild(document.createElement('br'));
 		}
 	}
+	//Creando el boton limpiar despues de las teclas numericas
 	let limpiar = document.createElement('button');
-	limpiar.textContent = "Limpiar";
+	limpiar.textContent = "Reset";
+	limpiar.type = 'button';
 	limpiar.addEventListener('click', ()=> {
 		barra.value = '';
 	});
 	teclado.appendChild(limpiar);
+	teclado.appendChild(document.createElement('br'));	
 }
 teclado();
+alerta();
