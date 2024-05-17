efc40e6d333eb0890dac0f89e039ebf746ed2363
DrN25 <rafael.nina123@gmail.com>
2024-05-17 16:23:28 -0500

    Creando una barra donde se mostraran los numeros ingresados por el usuario. Tambien se agrego un boton limpiar en el teclado, el cual al ser presionado este limpiara todo el contenido presente en la barra

--- Lab03/js/script_ejercicio1.js
+++ Lab03/js/script_ejercicio1.js
@@ -12,10 +12,23 @@ function teclado() {
 	let arr = [1,2,3,4,5,6,7,8,9,0];
 	arr = desordenarArray(arr);
 	let teclado = document.getElementById('teclado');
+	let barra = document.getElementById('barra');
 	for(let i = 0; i < arr.length; i++) {
 		let tecla = document.createElement('button');
 		tecla.textContent = arr[i];
 		teclado.appendChild(tecla);
+		tecla.addEventListener('click', ()=> {
+			barra.value += arr[i];
+		});
+		if( (i + 1) % 3 === 0) {
+			teclado.appendChild(document.createElement('br'));
+		}
 	}
+	let limpiar = document.createElement('button');
+	limpiar.textContent = "Limpiar";
+	limpiar.addEventListener('click', ()=> {
+		barra.value = '';
+	});
+	teclado.appendChild(limpiar);
 }
 teclado();
