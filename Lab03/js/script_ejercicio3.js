dfc407dc9e4b823ce728bb31d6c0348350ffe86e
DrN25 <rninacal@unsa.edu.pe>
2024-05-18 23:24:13 -0500

    Implementando el teclado de letras y la barra donde se actualizara la palabra actual, donde si se presiona una letra que pertenezca a la palabra, se actualizara, caso contrario se mantendran con _ los espacios no adivinados. De momento no hay limite en la cantidad de intentos

--- Lab03/js/script_ejercicio3.js
+++ Lab03/js/script_ejercicio3.js
@@ -10,5 +10,33 @@ function ahorcado() {
 	let palabra = randomArray(arr);
 	let barra = document.getElementById('barra');
 	let teclado = document.getElementById('teclado');
+	let palabraProgreso = '_'.repeat(palabra.length);
+	//Funcion que actualiza la palabra actual segun la palabra seleccionada
+	function barraActualizada(letra) {
+		let temp = '';
+		for(let i = 0; i < palabra.length; i++) {
+			if(palabra[i] === letra || palabraProgreso[i] !== '_') {
+				temp += palabra[i];
+			} else {
+				temp += "_";
+			}
+		}
+		palabraProgreso = temp;
+		barra.value = palabraProgreso;
+	}
+	//Se generan el abecedario y sus respectivos botones
+	for(let i = 0; i < abecedario.length; i++) {
+		let letra = abecedario[i];
+		let tecla = document.createElement('button');
+		tecla.textContent = letra;
+		tecla.type = 'button';
+		teclado.appendChild(tecla);
+		tecla.addEventListener('click', ()=> {
+			barraActualizada(letra);
+		});
+		if( (i + 1) % 10 === 0) {	//Cada 10 botones se hara salto de linea
+			teclado.appendChild(document.createElement('br'));
+		}
+	}
 }
-ahorcado();
+ahorcado()
