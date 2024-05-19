ca3fd8079abec69e7cd7fd43a9e36f967dc85a9b
DrN25 <rninacal@unsa.edu.pe>
2024-05-18 22:40:00 -0500

    Se hizo que se cree una tabla donde cada vez que se presione la tecla = se agregara la operacion realizada a la tabla ya mencionasa. Tambien se trabajaron los estilos de la pagina

--- Lab03/js/script_ejercicio2.js
+++ Lab03/js/script_ejercicio2.js
@@ -2,11 +2,15 @@ function calculadora() {
 	let arr = [7,8,9,'/','E',4,5,6,'*','C',1,2,3,'-','.',0,'(',')','+','='];
 	let calculadora = document.getElementById('calculadora');
 	let barra = document.getElementById('barra');	//Aqui se mostraran los datos y el resultado
+	let resultados = document.getElementById('resultados');	//Operaciones realizadas
+	let tablaResultados = document.createElement('table');
+    resultados.appendChild(tablaResultados);
 	//Creando las teclas numericas y asignandoles un valor al ser presionadas
 	for(let i = 0; i < arr.length; i++) {
 		let tecla = document.createElement('button');
 		tecla.textContent = arr[i];
 		tecla.type = 'button';
+		tecla.classList.add('tecla');
 		calculadora.appendChild(tecla);
 		tecla.addEventListener('click', ()=> {
 			//Se toman condiciones acerca de que hacer con cada boton			
@@ -16,6 +20,8 @@ function calculadora() {
 				barra.value = '';	//Elimina todo
 			} else if (arr[i] == '=') {
 				try {
+					let fila = tablaResultados.insertRow();
+                    fila.textContent = barra.value;
 					barra.value = eval(barra.value);
 				} catch(error) {
 					barra.value = 'ERROR';
