32918d48260889180d969f776bf4e3fc89c1062d
DrN25 <rninacal@unsa.edu.pe>
2024-05-25 16:56:58 -0500

    En index.js se creo la funcion crearArbol(), la cual se encargara de revisar los directorios creados en la carpeta actual, y luego revisara los ducumentos en cada una de estos, y en este proceso ira almacenando los titulos en una cadena en forma de arbol, y esta sera devuelta. Con esto se al definir una ruta get para enviar a la pagina web se llamara a esta funcion y se enviara la cadena resultante, dentro del tag <pre> para que se respeten los espacios y saltos de linea realizados, funcionando correctamente

--- Lab04/index.js
+++ Lab04/index.js
@@ -22,6 +22,31 @@ app.post('/submit', (req, res) => {
 	res.send('Archivo guardado correctamente.');
 });
 
+const crearArbol = (dirActual, espacio = '') => {
+	const atr = fs.statSync(dirActual);
+	if(atr.isDirectory()) {
+		let resultado = '';
+		const carpetas = fs.readdirSync(dirActual)
+			.filter(carpeta => carpeta != 'node_modules' && fs.statSync(path.join(dirActual, carpeta)).isDirectory());
+		for(const carpeta of carpetas) {
+			const rutaCarpeta = path.join(dirActual, carpeta);
+			resultado += `${espacio}${path.basename(rutaCarpeta)}\n`;
+			const archivos = fs.readdirSync(rutaCarpeta);
+			for(const archivo of archivos) {
+				resultado += `${espacio} ${archivo}\n`;
+			}
+		}
+		return resultado;
+	} else {
+		return `${espacio}${path.basename(dirActual)}\n`;
+	}
+};
+
+app.get('/arbol', (req, res) => {
+	const arbol = crearArbol(path.join(__dirname, 'agenda'));
+	res.send(`<pre>${arbol.replace(/\n/g, '<br>')}</pre>`);
+});
+
 app.listen(3000, () => {
 	console.log("Escuchando en: http://localhost:3000")
 });
