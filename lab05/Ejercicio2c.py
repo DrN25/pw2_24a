b8ce2ad9c696b8bc031fe524f92985b6f3cb45a2
DrN25 <rninacal@unsa.edu.pe>
2024-05-30 12:01:48 -0500

    Resolviendo el ejercicio 3. Se hizo uso de una parte del ejercicio 1 y el metodo verticalMirror()

---
+++ lab05/Ejercicio2c.py
@@ -0,0 +1,12 @@
+from interpreter import draw
+from chessPictures import *
+
+knightWhite = knight
+knightBlack = knight.negative()
+
+row1 = knightWhite.join(knightBlack)
+row2 = row1.verticalMirror()
+
+total = row2.up(row1)
+
+draw(total)
